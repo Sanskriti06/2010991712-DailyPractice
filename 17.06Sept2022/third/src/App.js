9b237236a8299320ba8c4adb6c03e329fe5498d3
Sanskriti06 <sanskriti1712.be20@chitkara.edu.in>
2022-09-06 11:27:37 +0530

    06 Sept 2022 ReactJS

---
+++ 17.06Sept2022/third/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from "./components/Day3React";
+function App() {
+  return (
+   <div className="container">
+   
+      <Header title1='Sanskriti'/>
+   </div>
+  );
+}
+export default App;
