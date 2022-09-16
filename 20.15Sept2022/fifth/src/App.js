bcc0cd26743b24f7161aac682a35492e70f49681
Sanskriti06 <sanskriti1712.be20@chitkara.edu.in>
2022-09-16 12:45:00 +0530

    15 Sept 2022 Reactjs and CSS

---
+++ 20.15Sept2022/fifth/src/App.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import './App.css';
+import Home from './Home';
+//import "bootstrap/dist/css/bootstrap.min.css";
+const App=()=> {
+  return (
+    <div className="App">
+      <h1>This is App component of this web site</h1>
+      <p>This is a paragraph</p>
+      <Home/>
+    </div>
+  );
+ }
+export default App;
+
